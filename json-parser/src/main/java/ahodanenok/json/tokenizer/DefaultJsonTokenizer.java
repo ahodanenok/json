44e92a34d86a8959a338ab8e93d706122edacec1
Andrei Hodanenok <xoda91@gmail.com>
2024-07-03 20:44:56 +0300

    Add reading of structural characters

--- json-parser/src/main/java/ahodanenok/json/tokenizer/DefaultJsonTokenizer.java
+++ json-parser/src/main/java/ahodanenok/json/tokenizer/DefaultJsonTokenizer.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 public final class DefaultJsonTokenizer implements JsonTokenizer {
 
     private final Reader reader;
+    private JsonToken token;
 
     public DefaultJsonTokenizer(Reader reader) {
         this.reader = reader;
@@ -12,11 +13,59 @@ public final class DefaultJsonTokenizer implements JsonTokenizer {
 
     @Override
     public boolean advance() {
-        return false;
+        try {
+            return doAdvance();
+        } catch (Exception e) {
+            // todo: custom exception
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean doAdvance() throws Exception {
+        int ch;
+        do {
+            ch = reader.read();
+        } while (ch != -1 && isWhitespace(ch));
+
+        if (ch == -1) {
+            token = null;
+            return false;
+        }
+
+        if (ch == 0x5B) {
+            token = JsonToken.BEGIN_ARRAY;
+        } else if (ch == 0x5D) {
+            token = JsonToken.END_ARRAY;
+        } else if (ch == 0x7B) {
+            token = JsonToken.BEGIN_OBJECT;
+        } else if (ch == 0x7D) {
+            token = JsonToken.END_OBJECT;
+        } else if (ch == 0x3A) {
+            token = JsonToken.NAME_SEPARATOR;
+        } else if (ch == 0x2C) {
+            token = JsonToken.VALUE_SEPARATOR;
+        } else {
+            // todo: custom exception?
+            throw new IllegalStateException("unknown char: " + ch);
+        }
+
+        return true;
+    }
+
+    private boolean isWhitespace(int ch) {
+        return ch == 0x20 // Space
+            || ch == 0x9  // Horizontal tab
+            || ch == 0xA  // Line feed or New line
+            || ch == 0xD; // Carriage return
     }
 
     @Override
     public JsonToken currentToken() {
-        return null;
+        if (token == null) {
+            // todo: custom exception
+            throw new IllegalStateException("no token");
+        }
+
+        return token;
     }
 }
--- json-parser/src/test/java/ahodanenok/json/tokenizer/DefaultJsonTokenizerTest.java
+++ json-parser/src/test/java/ahodanenok/json/tokenizer/DefaultJsonTokenizerTest.java
@@ -3,15 +3,73 @@ package ahodanenok.json.tokenizer;
 import java.io.StringReader;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DefaultJsonTokenizerTest {
 
-    @Test
-    public void testReadEmpty() throws Exception {
-        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(""));
+    @ParameterizedTest
+    @ValueSource(strings = { "", " ", "\t", "\r", "\n", "\r\t \n" })
+    public void testReadEmpty(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "[", "\t[", "\r[", " [", " \t \r\n \n[", " \n \r[\t " })
+    public void testReadBeginArray(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.BEGIN_ARRAY, tokenizer.currentToken().getType());
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "]", "\t]", "\r]", " ]", " \r\t \n \n]", " \t\n]  " })
+    public void testReadEndArray(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.END_ARRAY, tokenizer.currentToken().getType());
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "{", "\t{", "\r{", " {", " \r\r\t \n {", " \n \r{  \t" })
+    public void testReadBeginObject(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.BEGIN_OBJECT, tokenizer.currentToken().getType());
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "}", "\t}", "\r}", " }", " \r \n\t \n }", " \t\n }  \r " })
+    public void testReadEndObject(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.END_OBJECT, tokenizer.currentToken().getType());
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { ":", "\t:", "\r:", " :", " \r \t \n :", " \t \n :  \r " })
+    public void testReadNameSeparator(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.NAME_SEPARATOR, tokenizer.currentToken().getType());
+        assertFalse(tokenizer.advance());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { ",", "\t,", "\r,", " ,", " \r \t \n ,", " \t \n ,  \r " })
+    public void testReadValueSeparator(String s) throws Exception {
+        DefaultJsonTokenizer tokenizer = new DefaultJsonTokenizer(new StringReader(s));
+        assertTrue(tokenizer.advance());
+        assertEquals(TokenType.VALUE_SEPARATOR, tokenizer.currentToken().getType());
         assertFalse(tokenizer.advance());
     }
 }
