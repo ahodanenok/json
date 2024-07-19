62dd6feb10ecc33f2b621d8b6dfcdda1674aa9d2
Andrei Hodanenok <xoda91@gmail.com>
2024-07-19 20:02:07 +0300

    Implement writing of simple json values

--- json-writer/src/main/java/ahodanenok/json/writer/DefaultJsonValueWriter.java
+++ json-writer/src/main/java/ahodanenok/json/writer/DefaultJsonValueWriter.java
@@ -1,13 +1,41 @@
 package ahodanenok.json.writer;
 
+import java.io.IOException;
 import java.io.Writer;
 
 import ahodanenok.json.value.JsonValue;
+import ahodanenok.json.value.ValueType;
 
 public final class DefaultJsonValueWriter implements JsonValueWriter {
 
     @Override
     public void writeValue(JsonValue value, Writer writer) {
+        if (value == null) {
+            return;
+        }
 
+        try {
+            doWriteValue(value, writer);
+        } catch (IOException e) {
+            // todo: custom exception?
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doWriteValue(JsonValue value, Writer writer) throws IOException {
+        ValueType type = value.getType();
+        if (type.equals(ValueType.STRING)) {
+            writer.write('"');
+            writer.write(value.asString().getValue()); // todo: escape
+            writer.write('"');
+        } else if (type.equals(ValueType.NUMBER)) {
+            writer.write(Double.toString(value.asNumber().doubleValue())); // todo: does it format according to rfc
+        } else if (type.equals(ValueType.BOOLEAN)) {
+            writer.write(value.asBoolean().getValue() ? "true" : "false");
+        } else if (type.equals(ValueType.NULL)) {
+            writer.write("null");
+        } else {
+            throw new IllegalStateException(String.format("Unsupported value type '%s'", type));
+        }
     }
 }
--- json-writer/src/test/java/ahodanenok/json/writer/DefaultJsonValueWriterTest.java
+++ json-writer/src/test/java/ahodanenok/json/writer/DefaultJsonValueWriterTest.java
@@ -1,11 +1,75 @@
 package ahodanenok.json.writer;
 
+import java.io.StringWriter;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import ahodanenok.json.value.JsonBoolean;
+import ahodanenok.json.value.JsonNull;
+import ahodanenok.json.value.JsonNumber;
+import ahodanenok.json.value.JsonString;
+import ahodanenok.json.value.JsonValue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DefaultJsonValueWriterTest {
 
     @Test
-    public void test() {
+    public void testWriteNull() {
+        StringWriter writer = new StringWriter();
+        JsonValue value = new JsonNull();
+        JsonValueWriter jsonWriter = new DefaultJsonValueWriter();
+        jsonWriter.writeValue(value, writer);
+        assertEquals("null", writer.toString());
+    }
 
+    @Test
+    public void testWriteTrue() {
+        StringWriter writer = new StringWriter();
+        JsonValue value = new JsonBoolean(true);
+        JsonValueWriter jsonWriter = new DefaultJsonValueWriter();
+        jsonWriter.writeValue(value, writer);
+        assertEquals("true", writer.toString());
+    }
+
+    @Test
+    public void testWriteFalse() {
+        StringWriter writer = new StringWriter();
+        JsonValue value = new JsonBoolean(false);
+        JsonValueWriter jsonWriter = new DefaultJsonValueWriter();
+        jsonWriter.writeValue(value, writer);
+        assertEquals("false", writer.toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'',     \"\"",
+        "abc,     \"abc\"",
+        "'hello world',     \"hello world\""
+    })
+    public void testWriteString(String s, String expected) {
+        StringWriter writer = new StringWriter();
+        JsonValue value = new JsonString(s);
+        JsonValueWriter jsonWriter = new DefaultJsonValueWriter();
+        jsonWriter.writeValue(value, writer);
+        assertEquals(expected, writer.toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0,     0.0",
+        "1,    1.0",
+        "-1,     -1.0",
+        "12.34,    12.34",
+        "-523.7812,     -523.7812"
+    })
+    public void testWriteNumber(Double n, String expected) {
+        StringWriter writer = new StringWriter();
+        JsonValue value = new JsonNumber(n);
+        JsonValueWriter jsonWriter = new DefaultJsonValueWriter();
+        jsonWriter.writeValue(value, writer);
+        assertEquals(expected, writer.toString());
     }
 }
