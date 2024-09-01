30ed36db710e0a04d7f33d4a98f54c9707973c2e
Andrei Hodanenok <xoda91@gmail.com>
2024-09-01 17:09:12 +0300

    Implement JsonPointer.add

---
+++ jakarta-json-processing/src/main/java/ahodanenok/json/jp/JsonPointerImpl.java
@@ -0,0 +1,208 @@
+package ahodanenok.json.jp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import jakarta.json.JsonArray;
+import jakarta.json.JsonException;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonPointer;
+import jakarta.json.JsonStructure;
+import jakarta.json.JsonValue;
+
+final class JsonPointerImpl implements JsonPointer {
+
+    private static final String REF_ARRAY_AFTER_LAST = "-";
+
+    final List<String> tokens;
+
+    JsonPointerImpl(String pointer) {
+        char ch;
+        int pos = 0;
+        List<String> tokens = new ArrayList<>();
+        while (pos < pointer.length()) {
+            if (pointer.charAt(pos) != '/') {
+                throw new JsonException("Pointer is not valid: reference token must start with a '/'");
+            }
+
+            pos++;
+            StringBuilder sb = new StringBuilder();
+            while (pos < pointer.length() && (ch = pointer.charAt(pos)) != '/') {
+                if (ch == '~' && pos < pointer.length()) {
+                    if (pointer.charAt(pos + 1) == '0') {
+                        sb.append('~');
+                        pos += 2;
+                        continue;
+                    } else if (pointer.charAt(pos + 1) == '1') {
+                        sb.append('/');
+                        pos += 2;
+                        continue;
+                    }
+                }
+
+                sb.append(ch);
+                pos++;
+            }
+
+            tokens.add(sb.toString());
+        }
+
+        this.tokens = tokens;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends JsonStructure> T add(T target, JsonValue value) {
+        Objects.requireNonNull(target);
+        Objects.requireNonNull(value);
+
+        if (tokens.isEmpty()) {
+            if (target.getClass().isAssignableFrom(value.getClass())) {
+                return (T) value;
+            } else {
+                throw new JsonException(String.format(
+                    "Value of type '%s' is not assignable to the target of type '%s'",
+                    value.getClass().getCanonicalName(),
+                    target.getClass().getCanonicalName()));
+            }
+        }
+
+        if (tokens.size() == 1) {
+            return (T) add(target, tokens.get(0), value);
+        }
+
+        List<JsonStructure> path = resolveTarget(target);
+        JsonStructure newTarget = add(path.get(path.size() - 1), tokens.get(tokens.size() - 1), value);
+        for (int i = path.size() - 2; i >= 0; i--) {
+            newTarget = replace(path.get(i), tokens.get(i), newTarget);
+        }
+
+        return (T) newTarget;
+    }
+
+    private JsonStructure add(JsonStructure target, String ref, JsonValue value) {
+        if (target instanceof JsonObject) {
+            Map<String, JsonValue> newValues = new LinkedHashMap<>(target.asJsonObject());
+            newValues.put(ref, value);
+
+            return new JsonObjectImpl(newValues);
+        } else if (target instanceof JsonArray) {
+            List<JsonValue> newValues = new ArrayList<>(target.asJsonArray());
+            if (ref.equals(REF_ARRAY_AFTER_LAST)) {
+                newValues.add(value);
+            } else {
+                int idx = getArrayIndex(ref);
+                if (idx > newValues.size()) {
+                    throw new JsonException(String.format("Array index is out of range: %s > %s", idx, newValues.size()));
+                }
+
+                newValues.add(idx, value);
+            }
+
+            return new JsonArrayImpl(newValues);
+        } else {
+            throw new IllegalStateException("Uknown json structure: " + target.getClass());
+        }
+    }
+
+    @Override
+    public <T extends JsonStructure> T remove(T target) {
+        return null;
+    }
+
+    @Override
+    public <T extends JsonStructure> T replace(T target, JsonValue value) {
+        return null;
+    }
+
+    private JsonStructure replace(JsonStructure target, String ref, JsonValue value) {
+        if (target instanceof JsonObject) {
+            Map<String, JsonValue> newValues = new LinkedHashMap<>(target.asJsonObject());
+            newValues.put(ref, value);
+
+            return new JsonObjectImpl(newValues);
+        } else if (target instanceof JsonArray) {
+            List<JsonValue> newValues = new ArrayList<>(target.asJsonArray());
+            if (ref.equals(REF_ARRAY_AFTER_LAST)) {
+                throw new JsonException(String.format("Value at index '%s' doesn't exist", REF_ARRAY_AFTER_LAST));
+            } else {
+                int idx = getArrayIndex(ref);
+                if (idx >= newValues.size()) {
+                    throw new JsonException(String.format("Array index is out of range: %s >= %s", idx, newValues.size()));
+                }
+
+                newValues.set(idx, value);
+            }
+
+            return new JsonArrayImpl(newValues);
+        } else {
+            throw new IllegalStateException("Uknown json structure: " + target.getClass());
+        }
+    }
+
+    @Override
+    public boolean containsValue(JsonStructure target) {
+        return false;
+    }
+
+    @Override
+    public JsonValue getValue(JsonStructure target) {
+        return null;
+    }
+
+    private List<JsonStructure> resolveTarget(JsonStructure target) {
+        List<JsonStructure> path = new ArrayList<>(tokens.size());
+        JsonValue currentTarget = target;
+        Iterator<String> tokenIterator = tokens.iterator();
+        while (tokenIterator.hasNext()) {
+            String token = tokenIterator.next();
+            if (currentTarget instanceof JsonObject object) {
+                path.add(object);
+                if (tokenIterator.hasNext()) {
+                    currentTarget = object.get(token);
+                }
+            } else if (currentTarget instanceof JsonArray array) {
+                path.add(array);
+                if (tokenIterator.hasNext()) {
+                    if (token.equals(REF_ARRAY_AFTER_LAST)) {
+                        throw new JsonException(String.format("Value at index '%s' doesn't exist", REF_ARRAY_AFTER_LAST));
+                    }
+
+                    int idx = getArrayIndex(token);
+                    if (idx >= array.size()) {
+                        throw new JsonException(String.format("Array index is out of range: %s >= %s", idx, array.size()));
+                    }
+
+                    currentTarget = array.get(idx);
+                }
+            } else {
+                throw new JsonException(String.format(
+                    "Target must be an array or object, got '%s'",
+                    currentTarget.getValueType()));
+            }
+        }
+
+        return path;
+    }
+
+    private int getArrayIndex(String ref) {
+        if (ref.length() > 1 && ref.charAt(0) == '0') {
+            throw new JsonException("Array index can't start with 0");
+        }
+
+        try {
+            int idx = Integer.parseInt(ref);
+            if (idx < 0) {
+                throw new JsonException(String.format("Array index is out of range: %s < 0", idx));
+            }
+
+            return idx;
+        } catch (NumberFormatException e) {
+            throw new JsonException(String.format("Invalid array index: '%s'", ref));
+        }
+    }
+}
---
+++ jakarta-json-processing/src/test/java/ahodanenok/json/jp/JsonPointerImplTest.java
@@ -0,0 +1,239 @@
+package ahodanenok.json.jp;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.json.JsonArray;
+import jakarta.json.JsonException;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonPointer;
+import jakarta.json.JsonValue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JsonPointerImplTest {
+
+    @Test
+    public void testCreateJsonPointer() {
+        assertEquals(List.of(), new JsonPointerImpl("").tokens);
+        assertEquals(List.of(" "), new JsonPointerImpl("/ ").tokens);
+        assertEquals(List.of(" ", "  "), new JsonPointerImpl("/ /  ").tokens);
+        assertEquals(List.of("123"), new JsonPointerImpl("/123").tokens);
+        assertEquals(List.of("test"), new JsonPointerImpl("/test").tokens);
+        assertEquals(List.of("c%d"), new JsonPointerImpl("/c%d").tokens);
+        assertEquals(List.of("a/b"), new JsonPointerImpl("/a~1b").tokens);
+        assertEquals(List.of("m~n"), new JsonPointerImpl("/m~0n").tokens);
+        assertEquals(List.of("k\\\"l"), new JsonPointerImpl("/k\\\"l").tokens);
+        assertEquals(List.of("foo", "bar"), new JsonPointerImpl("/foo/bar").tokens);
+        assertEquals(List.of("foo", "0"), new JsonPointerImpl("/foo/0").tokens);
+        assertEquals(List.of("response", "0", "items", "25", "name"), new JsonPointerImpl("/response/0/items/25/name").tokens);
+    }
+
+    @Test
+    public void testAddObjectEmptyPointer() {
+        JsonObject a = new JsonObjectImpl(Map.of());
+        JsonObject b = new JsonObjectImpl(Map.of());
+        assertSame(b, new JsonPointerImpl("").add(a, b));
+    }
+
+    @Test
+    public void testAddArrayEmptyPointer() {
+        JsonArray a = new JsonArrayImpl(List.of());
+        JsonArray b = new JsonArrayImpl(List.of());
+        assertSame(b, new JsonPointerImpl("").add(a, b));
+    }
+
+    @Test
+    public void testErrorWhenAddDifferentTypesAtEmptyPointer() {
+        JsonArray a = new JsonArrayImpl(List.of());
+        JsonObject b = new JsonObjectImpl(Map.of());
+        JsonException e1 = assertThrows(JsonException.class, () -> new JsonPointerImpl("").add(a, b));
+        assertEquals(
+            "Value of type 'ahodanenok.json.jp.JsonObjectImpl' is not assignable to the target of type 'ahodanenok.json.jp.JsonArrayImpl'",
+            e1.getMessage());
+        JsonException e2 = assertThrows(JsonException.class, () -> new JsonPointerImpl("").add(b, a));
+        assertEquals(
+            "Value of type 'ahodanenok.json.jp.JsonArrayImpl' is not assignable to the target of type 'ahodanenok.json.jp.JsonObjectImpl'",
+            e2.getMessage());
+    }
+
+    @Test
+    public void testAddValueToArray() {
+        JsonArray array = new JsonArrayImpl(List.of(
+            new JsonNumberIntegerImpl(10),
+            new JsonNumberIntegerImpl(20),
+            new JsonNumberIntegerImpl(30)));
+        JsonPointer p0 = new JsonPointerImpl("/0");
+        JsonPointer p1 = new JsonPointerImpl("/2");
+        JsonPointer p2 = new JsonPointerImpl("/5");
+        JsonPointer p3 = new JsonPointerImpl("/-");
+
+        JsonArray result0 = p0.add(array, new JsonNumberIntegerImpl(1));
+        assertEquals(10, array.getInt(0));
+        assertEquals(20, array.getInt(1));
+        assertEquals(30, array.getInt(2));
+        assertEquals(1, result0.getInt(0));
+        assertEquals(10, result0.getInt(1));
+        assertEquals(20, result0.getInt(2));
+        assertEquals(30, result0.getInt(3));
+
+        JsonArray result1 = p1.add(result0, new JsonNumberIntegerImpl(2));
+        assertEquals(1, result0.getInt(0));
+        assertEquals(10, result0.getInt(1));
+        assertEquals(20, result0.getInt(2));
+        assertEquals(30, result0.getInt(3));
+        assertEquals(1, result1.getInt(0));
+        assertEquals(10, result1.getInt(1));
+        assertEquals(2, result1.getInt(2));
+        assertEquals(20, result1.getInt(3));
+        assertEquals(30, result1.getInt(4));
+
+        JsonArray result2 = p2.add(result1, new JsonNumberIntegerImpl(3));
+        assertEquals(1, result1.getInt(0));
+        assertEquals(10, result1.getInt(1));
+        assertEquals(2, result1.getInt(2));
+        assertEquals(20, result1.getInt(3));
+        assertEquals(30, result1.getInt(4));
+        assertEquals(1, result2.getInt(0));
+        assertEquals(10, result2.getInt(1));
+        assertEquals(2, result2.getInt(2));
+        assertEquals(20, result2.getInt(3));
+        assertEquals(30, result2.getInt(4));
+        assertEquals(3, result2.getInt(5));
+
+        JsonArray result3 = p3.add(result2, new JsonNumberIntegerImpl(4));
+        assertEquals(1, result2.getInt(0));
+        assertEquals(10, result2.getInt(1));
+        assertEquals(2, result2.getInt(2));
+        assertEquals(20, result2.getInt(3));
+        assertEquals(30, result2.getInt(4));
+        assertEquals(3, result2.getInt(5));
+        assertEquals(1, result3.getInt(0));
+        assertEquals(10, result3.getInt(1));
+        assertEquals(2, result3.getInt(2));
+        assertEquals(20, result3.getInt(3));
+        assertEquals(30, result3.getInt(4));
+        assertEquals(3, result3.getInt(5));
+        assertEquals(4, result3.getInt(6));
+    }
+
+    @Test
+    public void testAddValueToArrayInsideArray() {
+        JsonArray array = new JsonArrayImpl(List.of(
+            new JsonNumberIntegerImpl(10),
+            new JsonArrayImpl(List.of(
+                new JsonNumberIntegerImpl(20),
+                new JsonNumberIntegerImpl(30)
+            ))
+        ));
+        JsonPointer p = new JsonPointerImpl("/1/0");
+
+        JsonArray result = p.add(array, new JsonNumberIntegerImpl(1));
+        assertEquals(10, array.getInt(0));
+        assertEquals(20, array.get(1).asJsonArray().getInt(0));
+        assertEquals(30, array.get(1).asJsonArray().getInt(1));
+        assertEquals(10, result.getInt(0));
+        assertEquals(1, result.get(1).asJsonArray().getInt(0));
+        assertEquals(20, result.get(1).asJsonArray().getInt(1));
+        assertEquals(30, result.get(1).asJsonArray().getInt(2));
+    }
+
+    @Test
+    public void testAddValueToArrayInsideObject() {
+        JsonObject object = new JsonObjectImpl(new LinkedHashMap<>() {{
+            put("a", new JsonNumberIntegerImpl(100));
+            put("b", new JsonArrayImpl(List.of(
+                new JsonNumberIntegerImpl(10),
+                new JsonNumberIntegerImpl(20)
+            )));
+            put("c", new JsonNumberIntegerImpl(200));
+        }});
+        JsonPointer p = new JsonPointerImpl("/b/1");
+
+        JsonObject result = p.add(object, new JsonNumberIntegerImpl(1));
+        assertEquals(100, object.getInt("a"));
+        assertEquals(10, object.get("b").asJsonArray().getInt(0));
+        assertEquals(20, object.get("b").asJsonArray().getInt(1));
+        assertEquals(200, object.getInt("c"));
+        assertEquals(100, result.getInt("a"));
+        assertEquals(10, result.get("b").asJsonArray().getInt(0));
+        assertEquals(1, result.get("b").asJsonArray().getInt(1));
+        assertEquals(20, result.get("b").asJsonArray().getInt(2));
+        assertEquals(200, result.getInt("c"));
+    }
+
+    @Test
+    public void testAddValueToObject() {
+        JsonObject object = new JsonObjectImpl(new LinkedHashMap<>() {{
+            put("a", JsonValue.TRUE);
+            put("b", new JsonNumberIntegerImpl(100));
+            put("c", new JsonStringImpl("test"));
+        }});
+
+        JsonObject result0 = new JsonPointerImpl("/a").add(object, new JsonNumberIntegerImpl(500));
+        assertEquals(JsonValue.TRUE, object.get("a"));
+        assertEquals(100, object.getInt("b"));
+        assertEquals("test", object.getString("c"));
+        assertEquals(500, result0.getInt("a"));
+        assertEquals(100, result0.getInt("b"));
+        assertEquals("test", result0.getString("c"));
+
+        JsonObject result1 = new JsonPointerImpl("/bb").add(result0, JsonValue.FALSE);
+        assertEquals(500, result0.getInt("a"));
+        assertEquals(100, result0.getInt("b"));
+        assertEquals("test", result0.getString("c"));
+        assertEquals(500, result1.getInt("a"));
+        assertEquals(100, result1.getInt("b"));
+        assertEquals("test", result1.getString("c"));
+        assertEquals(JsonValue.FALSE, result1.get("bb"));
+    }
+
+    @Test
+    public void testAddValueToObjectInsideArray() {
+        JsonArray array = new JsonArrayImpl(List.of(
+            JsonValue.NULL,
+            new JsonNumberIntegerImpl(123),
+            new JsonObjectImpl(new LinkedHashMap<>() {{
+                put("foo", new JsonStringImpl("bar"));
+                put("a1", JsonValue.TRUE);
+            }})
+        ));
+
+        JsonArray result = new JsonPointerImpl("/2/abcd").add(array, new JsonNumberIntegerImpl(-3));
+        assertEquals(JsonValue.NULL, array.get(0));
+        assertEquals(123, array.getInt(1));
+        assertEquals("bar", array.get(2).asJsonObject().getString("foo"));
+        assertEquals(JsonValue.TRUE, array.get(2).asJsonObject().get("a1"));
+        assertEquals(JsonValue.NULL, result.get(0));
+        assertEquals(123, result.getInt(1));
+        assertEquals("bar", result.get(2).asJsonObject().getString("foo"));
+        assertEquals(JsonValue.TRUE, result.get(2).asJsonObject().get("a1"));
+        assertEquals(-3, result.get(2).asJsonObject().getInt("abcd"));
+    }
+
+    @Test
+    public void testAddValueToObjectInsideObject() {
+        JsonObject object = new JsonObjectImpl(new LinkedHashMap<>() {{
+            put("x", new JsonObjectImpl(new LinkedHashMap<>() {{
+                put("y", new JsonObjectImpl(new LinkedHashMap<>() {{
+                    put("abc", new JsonNumberIntegerImpl(100));
+                }}));
+            }}));
+            put("z", JsonValue.TRUE);
+        }});
+
+        JsonObject result = new JsonPointerImpl("/x/y/z").add(object, new JsonStringImpl("test"));
+        assertEquals(1, object.get("x").asJsonObject().get("y").asJsonObject().size());
+        assertEquals(100, object.get("x").asJsonObject().get("y").asJsonObject().getInt("abc"));
+        assertEquals(JsonValue.TRUE, object.get("z"));
+        assertEquals(2, result.get("x").asJsonObject().get("y").asJsonObject().size());
+        assertEquals(100, result.get("x").asJsonObject().get("y").asJsonObject().getInt("abc"));
+        assertEquals("test", result.get("x").asJsonObject().get("y").asJsonObject().getString("z"));
+        assertEquals(JsonValue.TRUE, result.get("z"));
+    }
+}
